d0516cd204b254985eaa001d373d5101470ad146
RKowerski <rileyko975@gmail.com>
2025-03-13 14:18:25 -0400

    03/13
    Setting up things

---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static int partition(int[] array, int low, int high){
+        return -1;
+    }
+
+    public static void swap(int array, int i, int j){
+
+    }
+
+    public static void quickSort(int array, int low, int high){
+
+    }
+}
