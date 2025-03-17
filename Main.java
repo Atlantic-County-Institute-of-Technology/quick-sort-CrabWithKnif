038f9e604bfc66e3bc2fae4503a39996dafc02c3
RKowerski <rileyko975@gmail.com>
2025-03-17 14:11:13 -0400

    03/17/25
    Added some code into several of the functions, now just trying to work out what's needed.

--- Main.java
+++ Main.java
@@ -1,17 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-
+        int[] aba = {2,4,5,1,7,9,3,6,8};
+        quickSort(aba, 0, aba.length-1);
     }
 
-    public static int partition(int[] array, int low, int high){
-        return -1;
+    public static int partition(int[] arr, int low, int high){
+        int pivot = arr[arr.length-1];
+        return pivot;
     }
 
-    public static void swap(int array, int i, int j){
-
+    public static void swap(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 
-    public static void quickSort(int array, int low, int high){
-
+    public static void quickSort(int[] arr, int low, int high){
+        
     }
+    /*int mid = (start+end)/2;
+        if(array[mid] < target)
+            return(bSearch(array, mid+1,end,target));
+        else if(array[mid] > target)
+            return bSearch(array, start,mid-1,target);
+        else
+            return mid;*/
 }
